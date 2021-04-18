288569e357f525fd914ad069c244dce03a365573
Melissa Franz <melissafranz@Melissas-MacBook-Pro.local>
2021-04-18 14:39:30 -0400

    Added a function to get all documents in a Firebase collection.

--- src/components/buySections/DepartmentList.jsx
+++ src/components/buySections/DepartmentList.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
 import * as S from './DepartmentListStyle';
 import { Department } from './department/Department';
+import { db } from '../../database/firebaseConfig';
 
 export const DepartmentList = ({ depInfo, clickedDept }) => {
+
+  //need to get all of the departments in Firebase
+  //display them onscreen as buttons
+
+  //gets one department from the University collection
+  var docRef = db.collection("University").doc("art_department");
+
+  docRef.get().then((doc) => {
+    if (doc.exists) {
+        console.log("Document data (from Firebase):", doc.data());
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+  }).catch((error) => {
+    console.log("Error getting document:", error); 
+  });
+  
+
+  //testing - gets all documents in a collection from Firebase
+  async function testing(){
+  const uniRef = db.collection('University');
+  const snapshot = await uniRef.get();
+  snapshot.forEach(doc => {
+    console.log(doc.id, '=>', doc.data());
+  });
+}
+  testing();
+
   return (
     <S.Container>
       {depInfo.map((dep) => (
--- src/pages/Sell.jsx
+++ src/pages/Sell.jsx
@@ -11,3 +11,4 @@ export default function Sell() {
     </div>
   );
 }
+
